59510dceb3f57106f4ab5adf39708b251622e807
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2024-11-14 13:12:23 -0600

    done
    done

--- pbp.ojj_rpg/Scripts/ClassSelect.gd
+++ pbp.ojj_rpg/Scripts/ClassSelect.gd
@@ -2,37 +2,32 @@ extends Node
 var pClass : playerClass
 var classes : Array
 var place: int
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pClass = playerClass.new()
 	classes = []
 	classes.resize(5)
 	classes[0] = playerClass.new()
-	classes[0].setClass("goku", 100, 20,20, "sword", 40, 50)
+	classes[0].setClass("Brawler", 100, 20,20, "Fists", 40, 50)
 	classes[1] = playerClass.new()
-	classes[1].setClass("luffy", 50, 10,6, "punch", 40, 50)
+	classes[1].setClass("Swordsman", 50, 10,6, "Sword", 40, 50)
 	classes[2] = playerClass.new()
-	classes[2].setClass("steve", 100, 20,8, "revolver", 40, 50)
+	classes[2].setClass("Gun Slinger", 100, 20,8, "Revolver", 40, 50)
 	classes[3] = playerClass.new()
-	classes[3].setClass("chase", 100, 20,20, "pistol", 40, 50)
+	classes[3].setClass("Engineer", 100, 20,20, "Wrench", 40, 50)
 	classes[4] = playerClass.new()
-	classes[4].setClass("goku jr", 100, 20,20, "punchy punch", 40, 50)
-	
+	classes[4].setClass("Marksman", 100, 20,20, "Sniper", 40, 50)
+	pClass = classes[0]
+	$"../CharacterStats".text = "Name: " + pClass.getName() + "\nWeapon: " + pClass.getWeaponName() + "\nHealth: " + str(pClass.getHealth()) + "\nStamina:" + str(pClass.getStamina()) + "\nMana:" + str(pClass.getMana()) 
 
 func _on_pressed() -> void:
 	if place == 4:
 		place = 0
 	else:
 		place += 1
-	print("next") # Replace with function body.
+	print("next")
 	pClass = classes[place]
-	print(pClass.getName())
-	print(pClass.getHealth())
-	print(pClass.getStamina())
-	print(pClass.getMana())
-	print(pClass.getWeaponName())
-	print(pClass.getWeaponDamage())
-	print(pClass.getWeaponSpeed())
+	$"../CharacterStats".text = "Name: " + pClass.getName() + "\nWeapon: " + pClass.getWeaponName() + "\nHealth: " + str(pClass.getHealth()) + "\nStamina:" + str(pClass.getStamina()) + "\nMana:" + str(pClass.getMana()) 
 	
 
 func _on_previous_button_pressed() -> void:
@@ -40,12 +35,6 @@ func _on_previous_button_pressed() -> void:
 		place = 4
 	else:
 		place -= 1
-	print("previous") # Replace with function body.
+	print("previous") 
 	pClass = classes[place]
-	print(pClass.getName())
-	print(pClass.getHealth())
-	print(pClass.getStamina())
-	print(pClass.getMana())
-	print(pClass.getWeaponName())
-	print(pClass.getWeaponDamage())
-	print(pClass.getWeaponSpeed())
+	$"../CharacterStats".text = "Name: " + pClass.getName() + "\nWeapon: " + pClass.getWeaponName() + "\nHealth: " + str(pClass.getHealth()) + "\nStamina:" + str(pClass.getStamina()) + "\nMana:" + str(pClass.getMana()) 
