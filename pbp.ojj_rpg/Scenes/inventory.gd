824a23fd1a8d890010b8ca7f05934438bf03077f
Owen Frakes <OwenBFrakes@gmail.com>
2025-01-17 13:06:04 -0600

    Player Menu & Item_Master

--- pbp.ojj_rpg/Scenes/inventory.gd
+++ pbp.ojj_rpg/Scenes/inventory.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @onready var item_list = $Panel/ItemList
-@onready var remove_button = $Panel/RemoveItemButton
+@onready var remove_button = $Panel/ButtonContainer/RemoveItemButton
+@onready var use_button = $Panel/ButtonContainer/UseButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
-	item_list.item_selected.connect(activate_remove_button)
+	item_list.item_selected.connect(activate_buttons)
+	PlayerStats.player_item_list = item_list.get_path()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,12 +20,23 @@ func _process(delta: float) -> void:
 		else:
 			item_list.deselect_all()
 			remove_button.disabled = true
+			use_button.disabled = true
 			show()
 			get_tree().paused = true
 
 # Signal attached to this, AddButton.
 func add_item():
 	item_list.add_item("BANANA")
+
+func use_item():
+	# Get the selected Item's name
+	var item_name = item_list.get_item_text(item_list.get_selected_items()[0])
+	use_button.disabled = true
+	
+	# Make the item master and have it work its magic.
+	var item_master = ItemMaster.new()
+	add_child(item_master)
+	item_master.find_item(item_name)
 
 func remove_item():
 	var selected_item = item_list.get_selected_items()
@@ -35,5 +48,6 @@ func remove_item():
 		item_list.remove_item(selected_item)
 		remove_button.disabled = true
 
-func activate_remove_button(index : int):
+func activate_buttons(index : int):
+	use_button.disabled = false
 	remove_button.disabled = false
---
+++ pbp.ojj_rpg/Scripts/item_master.gd
@@ -0,0 +1,21 @@
+class_name ItemMaster
+extends Node
+
+@onready var item_list_reference
+@onready var player_reference
+
+func _ready() -> void:
+	item_list_reference = get_node(PlayerStats.player_item_list)
+	player_reference = get_node(PlayerStats.player_node_path)
+
+func find_item(item_name : String):
+	item_name = item_name.to_lower()
+	if(item_name.match("")):
+		print("Error: No Item Name - item_master.gd")
+	else:
+		var selected_item = item_list_reference.get_selected_items()[0]
+		item_list_reference.remove_item(selected_item)
+		match(item_name):
+			"banana":
+				print("ANANAAAAA")
+				player_reference.player_sprite.modulate = Color(0,1,0)
