356a2e70967a21e67737e9198bbcdcd84afdd89c
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2025-01-27 12:46:16 -0600

    Brought over the weapon

--- pbp.ojj_rpg/Scripts/Weapon.gd
+++ pbp.ojj_rpg/Scripts/Weapon.gd
@@ -3,11 +3,13 @@ extends Node
 var wName
 var damage
 var attackSpeed
+var type
 
-func setWeapon(tempName: String, tempDamage: float, tempSpeed: float):
+func setWeapon(tempName: String, tempDamage: float, tempSpeed: float, tempType: String):
 	wName = tempName
 	damage = tempDamage
 	attackSpeed = tempSpeed
+	type = tempType
 
 #get Variables
 func getName():
@@ -16,6 +18,8 @@ func getDamage():
 	return damage
 func getAttackSpeed():
 	return attackSpeed
+func getType():
+	return type
 
 #set Variables
 func setName(tempName: String):
