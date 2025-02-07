6426af9f8d2efa495c5477cf68736d1d1aebd994
Owen Frakes <OwenBFrakes@gmail.com>
2025-02-07 12:54:01 -0600

    Tweaking Out 2

--- pbp.ojj_rpg/Scripts/player_stats.gd
+++ pbp.ojj_rpg/Scripts/player_stats.gd
@@ -12,3 +12,11 @@ func stringInfo() -> String:
 		   str(selected_player_class.getHealth()) + "\n" + \
 		   str(selected_player_class.getStamina()) + "\n" + \
 		   str(selected_player_class.getMana()) + "\n"
+
+func _input(event: InputEvent) -> void:
+	if(Input.is_action_just_pressed("Fullscreen_Button")):
+		if(DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN):
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+	
