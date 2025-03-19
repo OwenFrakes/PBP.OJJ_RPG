5eb78531112bf6d096ba5283874f76cf9f170b32
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2025-03-19 12:46:40 -0500

    idk why this no work

--- pbp.ojj_rpg/Scripts/Interactables/tutorial_button.gd
+++ pbp.ojj_rpg/Scripts/Interactables/tutorial_button.gd
@@ -7,15 +7,59 @@ extends "res://Scripts/Interactables/interactable.gd"
 @onready var pic = $"../Player/PlayerCamera/Control/LorePannel/ItemPic"
 @onready var itemName = $"../Player/PlayerCamera/Control/LorePannel/ItemName"
 @onready var itemInfo = $"../Player/PlayerCamera/Control/LorePannel/ItemInfo"
+var count = 0
+var disks: Array
+var firstTime: bool
+
+func setDisks():
+	disks.resize(10)
+	print(disks.size())
+	disks[0] = Disk.new()
+	disks[0].setDisk("Disk 1", "res://Resources/Character/dante.png", "This is disk 1")
+	disks[1] = Disk.new()
+	disks[1].setDisk("Disk 2", "res://Resources/icon.svg", "This is disk 2")
+	disks[2] = Disk.new()
+	disks[2].setDisk("Disk 3", "res://Resources/Tree-2.png.png", "This is disk 3")
+	disks[3] = Disk.new()
+	disks[3].setDisk("Disk 4", "res://Resources/Grass1-3.png.png", "This is disk 4")
+	disks[4] = Disk.new()
+	disks[4].setDisk("Disk 5", "res://Resources/cryochamber-2.png.png", "This is disk 5")
+	disks[5] = Disk.new()
+	disks[5].setDisk("Disk 6", "res://Resources/Character/dante.png", "This is disk 6")
+	disks[6] = Disk.new()
+	disks[6].setDisk("Disk 7", "res://Resources/Character/dante.png", "This is disk 7")
+	disks[7] = Disk.new()
+	disks[7].setDisk("Disk 8", "res://Resources/Character/dante.png", "This is disk 8")
+	disks[8] = Disk.new()
+	disks[8].setDisk("Disk 9", "res://Resources/Character/dante.png", "This is disk 9")
+	disks[9] = Disk.new()
+	disks[9].setDisk("Disk 10", "res://Resources/Character/dante.png", "This is disk 10")
 
 func interact():
-	itemName.text = "working"
-	itemInfo.text = "nuh uh"
-	pic.texture = load("res://Resources/Character/dante.png")
-	if lorePan.visible:
-		lorePan.hide()
+	if firstTime:
+		setDisks()
+		firstTime = false
+	
+	
+	if(count > 9):
+		count = 0
+		itemName.text = disks[count].dName
+		itemInfo.text = disks[count].discript
+		pic.texture = disks[count].pic
+		if lorePan.visible:
+			lorePan.hide()
+		else:
+			lorePan.show()
+		count += 1
 	else:
-		lorePan.show()
+		itemName.text = disks[count].dName
+		itemInfo.text = disks[count].discript
+		pic.texture = disks[count].pic
+		if lorePan.visible:
+			lorePan.hide()
+		else:
+			lorePan.show()
+		count += 1
 
 	if particles.emitting:
 		pass
@@ -23,3 +67,4 @@ func interact():
 		animation.play("default")
 		await animation.animation_finished
 		particles.emitting = true
+	
